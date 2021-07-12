d32f8f27656ecdbdff55e37bbbf8a9a140b01605
Egor Gorskikh <egorgorskikh@gmail.com>
2021-07-12 17:17:09 +0300

    feat: completed 02-planning-a-paged-app

--- Section 1. HIITFit/02-planning-a-paged-app/HIITFit/HIITFit/Views/ContentView.swift
+++ Section 1. HIITFit/02-planning-a-paged-app/HIITFit/HIITFit/Views/ContentView.swift
@@ -32,11 +32,22 @@
 
 import SwiftUI
 
-@main
-struct HIITFitApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+struct ContentView: View {
+
+    var body: some View {
+        TabView {
+            WelcomeView()
+            ForEach(0 ..< 4) { number in
+                ExerciseView(index: number)
+            }
         }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
